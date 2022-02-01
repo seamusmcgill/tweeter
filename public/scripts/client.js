a9dbcccc1809b36b5b09d23c48eec5ea7fc896b6
Seamus McGill <seamusmcgill@gmail.com>
2022-02-01 18:55:43 -0500

    Add createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div>
+          <img src=${tweet["user"].avatars}></i>
+          <p>${tweet["user"].name}</p>
+        </div>
+        <p>${tweet["user"].handle}</p>
+      </header>
+      <p>${tweet["content"].text}</p>
+      <hr>
+      <footer>
+        <p>${timeago.format(tweet["created_at"])}</p>
+        <div>
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </footer>
+    </article>`);
+    
+    return $tweet;
+  };
+
+});
 
