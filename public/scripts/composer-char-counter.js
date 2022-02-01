ada0c868710becfb05d07a23c308fc80365e8c6b
Seamus McGill <seamusmcgill@gmail.com>
2022-02-01 12:25:47 -0500

    Update character counter dynamically with input

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("Document ready son!");
+
+  $("#tweet-text").on("input", function() {
+
+    // Set the initital characters to 140 and dynamically update the HTML counter
+    let characters = 140 - $(this).val().length;
+
+    $(this).siblings("output").html(characters);
+    
+    // Add class to change text color to red if negative characters left
+    if (characters < 0) {
+      $(this).siblings("output").addClass("over-limit");
+    } else {
+      $(this).siblings("output").removeClass("over-limit");
+    }
+  });
+
+});
