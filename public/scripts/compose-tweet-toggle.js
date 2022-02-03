d4b79d9a444a8fe50911d2fdd1237cd6a8629704
Seamus McGill <seamusmcgill@gmail.com>
2022-02-03 16:26:19 -0500

    Adjust comments

--- public/scripts/compose-tweet-toggle.js
+++ public/scripts/compose-tweet-toggle.js
@@ -1,5 +1,10 @@
 /* eslint-disable no-undef */
+// Logic to show/hide the compose tweet and toggle buttons
 $(document).ready(function() {
+
+  // Hide the toggle scroll button on pageload
+  $(".container > div.toggle-button").hide();
+
   // When scrolling past the height of the nav in mobile view, toggle visibility of nav and togglebutton
   $(window).scroll(function() {
     if ($(this).scrollTop() > 120 && $(window).width() <= 1024) {
@@ -9,9 +14,10 @@ $(document).ready(function() {
       $("nav.navbar-top div.navbar-top-nav").show();
       $(".container > div.toggle-button").fadeOut(200);
     }
+
   });
 
-  // Scroll up and slide down the new-tweet div on toggle button click
+  // Scroll to the top and slide down the new-tweet div on toggle button click
   $("main.container > div.toggle-button").on("click", function() {
 
     $("html, body").animate({scrollTop: 0 }, "slow");
